d27d680b37098cac96c03a37af0a17ccc7b71a2b
AlickOdala <161893895+AlickOdala@users.noreply.github.com>
2026-08-18 18:36:40 +0200

    Update welcome.tsx

--- src/pages/home/welcome.tsx
+++ src/pages/home/welcome.tsx
@@ -36,14 +36,12 @@ const Welcome = ({ data }: { data: any }) => {
 
   const variant = {
     container: {
-      backgroundImage: "linear-gradient(to top,  #12121290, transparent)",
       color: "primary.contrastText",
-      justifyContent: { xs: "space-between", sm: "none !important" },
       px: 2,
       pt: { xs: 12, sm: 12 },
       pb: 4,
-      minHeight: "100vh",
-      flexFlow: { xs: "column" },
+      height: "100%",
+   
     },
   };
 
