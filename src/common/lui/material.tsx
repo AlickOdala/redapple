679e3c0bc26d8b2642b5ca99acd2b9baf532653e
Alick Odala <odalaallick@gmail.com>
2026-08-19 18:31:56 +0200

    resolved gallery

--- src/common/lui/material.tsx
+++ src/common/lui/material.tsx
@@ -166,7 +166,7 @@ export const LuiCard = ({ children, shadow, ratio }: CardProps) => {
       className="p-rel"
       sx={{
         width: "100%",
-        aspectRatio ratio,
+        aspectRatio: ratio,
         borderRadius: "16px",
         pt: 3.5,
         px: 2,
@@ -178,7 +178,7 @@ export const LuiCard = ({ children, shadow, ratio }: CardProps) => {
         display: "flex",
         flexFlow: "column",
         gap: 2,
-        height:"auto", 
+        height: "auto",
         //backdropFilter: "blur(5px)",
       }}
     >
